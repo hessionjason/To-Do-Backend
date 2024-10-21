f359d940da9be3d8884a19b1f3c755917d78336f
jhession <jason.hession@amcsgroup.com>
2024-10-21 16:41:01 +0100

    Prepare for deployment

--- index.js
+++ index.js
@@ -47,14 +47,6 @@ app.put('/todos/:id', async (req, res) => {
     res.json(updatedTodo);
 });
 
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'frontend/build'))); // Adjust path if needed
-
-// The "catchall" handler: for any request that doesn't match one above, send back the React app
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
-});
-
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
